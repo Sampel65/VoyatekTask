3a19e3129c7362f58dad37c49fc339edc14d146a
sampel65 <sampeldan65@gmail.com>
2025-02-20 18:20:01 +0100

    Initial Commit

---
+++ VoyatekTask/VoyatekTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyatekTaskApp.swift
+//  VoyatekTask
+//
+//  Created by Samson Oluwapelumi on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct VoyatekTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
